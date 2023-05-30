c5819d98a9048b9f9c658b794f75404d272a6354
damac53 <mitchellefetty@gmail.com>
2023-05-30 16:03:14 +0200

    changes to the gaming app added

--- app/src/main/java/com/technical/mymathgame/MainActivity.kt
+++ app/src/main/java/com/technical/mymathgame/MainActivity.kt
@@ -2,10 +2,26 @@ package com.technical.mymathgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
+
+lateinit var addition : Button
+lateinit var subtraction : Button
+lateinit var multiplication : Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        addition = findViewById(R.id.buttonAdd)
+        subtraction = findViewById(R.id.buttonSub)
+        multiplication = findViewById(R.id.buttonMulti)
+
+
+        addition.setOnClickListener {
+           val intent = Intent(this@MainActivity,GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
