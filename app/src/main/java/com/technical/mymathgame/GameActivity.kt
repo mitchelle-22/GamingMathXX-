d5e5e0dcdbe66a69550d6b7c7fcada890908a66d
damac53 <mitchellefetty@gmail.com>
2023-06-01 13:02:28 +0200

    functionality added to the ok button

--- app/src/main/java/com/technical/mymathgame/GameActivity.kt
+++ app/src/main/java/com/technical/mymathgame/GameActivity.kt
@@ -2,10 +2,42 @@ package com.technical.mymathgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class GameActivity : AppCompatActivity() {
+
+    lateinit var textScore : TextView
+    lateinit var textLife : TextView
+    lateinit var textTime : TextView
+
+    lateinit var textQuestion : TextView
+    lateinit var textAnswer : TextView
+
+    lateinit var buttonOk : Button
+    lateinit var buttonNext : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_page)
+
+        textScore = findViewById(R.id.textViewScore)
+        textLife = findViewById(R.id.textViewLife)
+        textTime = findViewById(R.id.textViewTime)
+        textQuestion = findViewById(R.id.textViewQuestion)
+        textAnswer = findViewById(R.id.editTextAnswer)
+        buttonOk = findViewById(R.id.buttonOk)
+        buttonNext = findViewById(R.id.buttonNext)
+
+        buttonOk.setOnClickListener {
+
+        }
+        buttonNext.setOnClickListener {
+
+        }
+
+        fun gameContinue()
+        {
+
+        }
     }
 }
