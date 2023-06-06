ac6651864693801ee9fbe0bd090a976e50f44d39
damac53 <mitchellefetty@gmail.com>
2023-06-06 15:57:17 +0200

    Results activity added

--- app/src/main/java/com/technical/mymathgame/ResultActivity.kt
+++ app/src/main/java/com/technical/mymathgame/ResultActivity.kt
@@ -2,10 +2,37 @@ package com.technical.mymathgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.content.Intent
+
 
 class ResultActivity : AppCompatActivity() {
+
+    lateinit var result : TextView
+    lateinit var playAgain : Button
+    lateinit var exit : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        result = findViewById(R.id.textViewResults)
+        playAgain = findViewById(R.id.buttonAgain)
+        exit = findViewById(R.id.buttonExit)
+
+        playAgain.setOnClickListener {
+            val intent = Intent(this@ResultActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        exit.setOnClickListener {
+
+            //standard code to close an application
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_HOME)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
+
     }
 }
